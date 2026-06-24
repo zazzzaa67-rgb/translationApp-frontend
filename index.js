1fc4a195b5b14cb344838f4157ab2dcd47dcb9ce
zazzzaa67-rgb <z.azz.z.aa.67@gmail.com>
2026-06-24 21:39:16 +0300

    update index.js

--- index.js
+++ index.js
@@ -6,28 +6,29 @@ transBtn.addEventListener("click" ,async function(){
         const translang = document.getElementById("usLang").value
         let html = ''
         if(userText && translang){
-        try{
-        translation.innerHTML = `
-        <p class="loading">Loading...</p>`
-        const res= await fetch("https://translation-app-backend-five.vercel.app/api/chat" , {
-            method : "POST",
-            headers:  {  'Content-Type': 'application/json',
-        'X-Custom-Header': 'CustomValue'},
-            body : JSON.stringify({prompt :userText,lang : translang})
-        })
-        const data = await res.json()
-        console.log(data)
-        html = data.replay
-        transBtn.textContent = "Start Over"
-        transBtn.id = "Start-Over"
-        
-        
-    }catch(error){
-            console.error("An error occurred")
-            html = "</p class='wrong text'>Opps , server proplem</p>"
-        }
+            try{
+            translation.innerHTML = `
+            <p class="loading">Loading...</p>`
+            const res= await fetch("https://translation-app-backend-five.vercel.app/api/chat" , {
+                method : "POST",
+                headers:  {  'Content-Type': 'application/json',
+            'X-Custom-Header': 'CustomValue'},
+                body : JSON.stringify({prompt :userText,lang : translang})
+            })
+            const data = await res.json()
+            console.log(data)
+            html = data.replay
+            transBtn.textContent = "Start Over"
+            transBtn.id = "Start-Over"
+            
+            
+        }catch(error){
+                console.error("An error occurred")
+                html = "</p class='wrong text'>Opps , server proplem</p>"
+            }
     }else{
         html = "<p class='text select'>Please,select the your text and the language</p>"
+        translation.innerHTML = html
         setTimeout(function(){
             translation.innerHTML = select.innerHTML
         } , 800)
