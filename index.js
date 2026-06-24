4398cd8de6adcc560ecda0fe5c367caaf8e43ff1
zazzzaa67-rgb <z.azz.z.aa.67@gmail.com>
2026-06-24 18:57:37 +0300

    commit files

---
+++ index.js
@@ -0,0 +1,4 @@
+const userText = document.getElementById("userText")
+const translang = document.getElementById("usLang")
+const transBtn = document.getElementById("transBtn")
+const translation = document.getElementById("theTranslation")
